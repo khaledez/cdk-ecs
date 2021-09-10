36b6275fa70ac639632b81c107ebac0205cfc19c
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2021-09-10 16:32:52 +0300

    update build shell

--- promethus-nodejs/build.sh
+++ promethus-nodejs/build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -e
 
-image_domain='427368570714.dkr.ecr.ca-central-1.amazonaws.com'
+account_id="427368570714"
+aws_region="ca-central-1"
+
+image_domain="$account_id.dkr.ecr.$aws_region.amazonaws.com"
 image_name="prometheus-nodejs"
 
 docker buildx build --platform linux/amd64 -t "$image_domain/$image_name:latest" .
+
+aws ecr get-login-password --region $aws_region | docker login --username AWS --password-stdin "$image_domain"
 
 docker push "$image_domain/$image_name:latest"
